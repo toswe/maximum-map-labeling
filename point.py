945e4cdca03316815d916b1f929c8f8409298c5d
Mihajlo Zivkovic <mihajlozivkovic97@gmail.com>
2023-03-17 19:08:32 +0100

    Add Point class

---
+++ point.py
@@ -0,0 +1,17 @@
+import math
+
+
+class Point:
+    def __init__(self ,x, y):
+        self.x = x
+        self.y = y
+        self.ne : math.inf
+        self.nw : math.inf
+        self.se : math.inf
+        self.sw : math.inf
+    
+    def distance(self, point): 
+        return min(abs(self.x - point.x),abs(self.y-point.y))
+    
+    
+
