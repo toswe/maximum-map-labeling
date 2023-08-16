0e7beb7dd33d1bf3c79594c7b7b9997e8ed9c8f7
mi16087 <mihajlozivkovic97@gmail.com>
2023-08-16 13:08:55 +0200

    Update return of phase 2 of B algorythm

--- searches/b.py
+++ searches/b.py
@@ -41,7 +41,18 @@ class B(Search):
                     sq = Square(point2, square.get_opposite_orientation(), size)
                     if sq in point2.squares:
                         point2.squares.remove(sq)
+
+        for point1, point2 in itertools.combinations(self.points, 2):
+            for square in point1.squares:
+                if square.has_point(point2):
                     point1.squares.remove(square)
+        
+        # for point1 in self.points:
+        #     for point2 in self.points:
+        #         for square in point1.squares:
+        #             if square.has_point(point2):
+        #                 if square in point1.squares:
+        #                     point1.squares.remove(square)
 
         square_list = []
         for point in self.points:
@@ -63,40 +74,49 @@ class B(Search):
         conflicts, non_conflicts = self._phase_1(size)
         stack = []
         stack_good = []
-        for p in self.points:
-            stack.append(p)
-            pop = False
-            for point in stack:
-                if len(point.squares) == 0:
-                    return False
+        # print(".................")
+        # print(size)
+        for i in range(0,2):
+            for p in self.points:
+                stack.append(p)
+                # pop = False
+                # print(f"{p}, num_sq: {len(p.squares)}, in_non_conflicts: {p in non_conflicts}, ")
+                for point in stack:
+                    if len(point.squares) == 0:
+                        return False
 
-                elif point in non_conflicts:
-                    if non_conflicts[point]:
-                        chosen_square = random.choice(non_conflicts[point])
-                        for square in point.squares:
-                            if square != chosen_square:
-                                conflicts = B.remove_candidat(square, conflicts)
-                        if point == stack[-1]:
-                            pop = True
+                    elif point in non_conflicts:
+                        if non_conflicts[point]:
+                            chosen_square = random.choice(non_conflicts[point])
+                            for square in point.squares:
+                                if square != chosen_square:
+                                    conflicts = B.remove_candidat(square, conflicts)
+                                    # print(f"no_conflict :{point} , {square.orientation}")
+                            stack.remove(point)
                             stack_good.append(chosen_square)
 
-                elif len(point.squares) == 1:
-                    for square in conflicts[point.squares[0]]:
-                        if square in stack_good:
-                            return False
-                        conflicts = B.remove_candidat(square, conflicts)
-                    if point == stack[-1]:
-                        pop = True
-                        stack_good.append(square)
+                    elif len(point.squares) == 1:
+                        for square in conflicts[point.squares[0]]:
+                            if square in stack_good:
+                                return False
+                            conflicts = B.remove_candidat(square, conflicts)
+                        if point not in non_conflicts:
+                            non_conflicts[point] = []
+                        non_conflicts[point].append(point.squares[0])
 
-                else:
-                    for square in point.squares:
-                        for sq1, sq2 in itertools.combinations(conflicts[square], 2):
-                            if sq1.point == sq2.point and len(sq1.point.squares) == 2:
-                                B.remove_candidat(square, conflicts)
-                                break
-            if pop:
-                stack.pop()
+                            # print(f"last_one: {square.point} , {square.orientation}")
+                        # stack.remove(point)
+                        # stack_good.append(square)
+
+                    else:
+                        for square in point.squares:
+                            for sq1, sq2 in itertools.combinations(conflicts[square], 2):
+                                if sq1.point == sq2.point and len(sq1.point.squares) == 2:
+                                    B.remove_candidat(square, conflicts)
+                                    # print(f"overlaps 2 finals: {square.point} , {square.orientation}")
+                                    break
+            # if pop:
+            #     stack.pop()
 
         for point in self.points:
             if len(point.squares) == 0:
@@ -105,9 +125,11 @@ class B(Search):
                 hold = point.squares[0]
                 for square in point.squares:   
                     B.remove_candidat(square, conflicts)
+                    # print(f"after: {point} , {square.orientation}")
                 point.squares.append(hold)
+                # print(f"hold : {point} , {hold.orientation}")
 
-        square_orientation = [point.squares[0].orientation for point in self.points]
+        square_orientation = [point.squares[0] for point in self.points]
 
         return square_orientation
 
